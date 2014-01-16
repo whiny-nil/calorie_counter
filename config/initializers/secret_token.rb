eaabfe4f26481e2894b019c0232c4e28c7021af9
Marc Kelsey <marc.kelsey@gmail.com>
2014-01-15 22:01:19 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalorieCounter::Application.config.secret_key_base = '42ea29af78c93c4d3204d4890d9fbf5a76913402cce957378577c04a55b223637f9054f21648af5543a9e9a58cf5338dda470da0c96f8b93f8d494cd1d36580e'
